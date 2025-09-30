9ac01a60db87f361b9427efae0fa5bf3f1c06809
ezrrrr <hip77@naver.com>
2025-09-30 15:07:03 +0900

    azure 배포 시 오류건 설정 수정
    azure 배포 시 오류건 설정 수정

--- streamlit.sh
+++ streamlit.sh
@@ -1,2 +1,2 @@
-pip install streamlit
-python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
+pip install -r requirements.txt
+streamlit run app.py --server.port 8000 --server.address 0.0.0.0
